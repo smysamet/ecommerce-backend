cbbefb963cfed684171cd78651159c471cb5cde9
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-11-07 14:52:35 +0300

    Create Category Entity

---
+++ src/main/java/com/smy/ecommerce/entities/concretes/Category.java
@@ -0,0 +1,23 @@
+package com.smy.ecommerce.entities.concretes;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+}
