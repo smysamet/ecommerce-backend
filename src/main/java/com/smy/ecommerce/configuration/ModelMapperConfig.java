2a6fa4a6da74dd13cc508ed60b66d47ac3259652
Samet YUKSEL <sametmertyukselmlg@gmail.com>
2023-11-07 20:35:13 +0300

    Create ModelMapper Config Class

---
+++ src/main/java/com/smy/ecommerce/configuration/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.smy.ecommerce.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
